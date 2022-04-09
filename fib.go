904ce46e32ff07ef9e4e4bb5bdef1bd885d72cc1
Ondřej Benkovský <ondrej.benkovsky@jamf.com>
2022-04-09 11:54:47 +0200

    fib with memoization

--- fib.go
+++ fib.go
@@ -1,10 +1,15 @@
 package benchmark_action_poc
 
 func Fib(n int) int {
-	if n <= 1 {
-		return n
+	prevFibs := []int{0, 1}
+	for i := 3; i <= n; i++ {
+		nextFib := prevFibs[0] + prevFibs[1]
+		prevFibs = []int{prevFibs[1], nextFib}
 	}
-	return Fib(n-1) + Fib(n-2)
+	if n > 1 {
+		return prevFibs[1]
+	}
+	return prevFibs[0]
 }
 
 func Reverse(s string) string {
